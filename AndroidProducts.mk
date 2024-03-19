e737843b21793796044f4f32121f476a54311dba
velinxd <153659670+velinxd@users.noreply.github.com>
2024-03-19 17:25:25 +0700

    Bring up Octavi OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX2185.mk
+    $(LOCAL_DIR)/octavi_RMX2185.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX2185-user \
-    lineage_RMX2185-userdebug \
-    lineage_RMX2185-eng
+    octavi_RMX2185-user \
+    octavi_RMX2185-userdebug \
+    octavi_RMX2185-eng
