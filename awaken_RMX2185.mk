70922f7835c4c14f3c4b4368b40aaf68b1879330
velinxd <153659670+velinxd@users.noreply.github.com>
2024-03-20 14:53:25 +0700

    Update awaken_RMX2185.mk

--- awaken_RMX2185.mk
+++ awaken_RMX2185.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2185/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common.mk)
 
 # Awaken flags
 AWAKEN_BUILD_TYPE := COMMUNITY
